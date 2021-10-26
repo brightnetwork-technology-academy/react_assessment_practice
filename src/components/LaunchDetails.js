9a491585f9ff0bb4f81ddfcfef0459df154d4593
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2021-10-26 14:21:34 +0100

    add mission status check

--- src/components/LaunchDetails.js
+++ src/components/LaunchDetails.js
@@ -8,7 +8,7 @@ class LaunchDetails extends React.Component{
             <>
                 <h2>Launch Number {this.props.launch.flight_number}</h2>
                 <p><b>Date:</b> {new Date(this.props.launch.date_unix * 1000).toDateString()}</p>
-                <p><b>Successful Flight:</b> {this.props.launch.success ? "Yes" : "No"}</p>
+                <p><b>Mission Status:</b> {this.props.launch.upcoming ? "Scheduled" : (this.props.launch.success ? "Successful" : "Failed")}</p>
                 <p><b>Summary:</b> {this.props.launch.details}</p>
                 <a href={this.props.launch.links.article}>Find out more</a>
             </>
